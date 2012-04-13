aec83a3c76799b5d8a43af4c65191519a4ee2e59
Igor Korsunov <elquendi@yandex.ru>
2012-04-13 22:48:10 +0400

    add scroling

--- src/su/eqx/iggdrasil/life/GameView.java
+++ src/su/eqx/iggdrasil/life/GameView.java
@@ -2,10 +2,7 @@ package su.eqx.iggdrasil.life;
 
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
+import android.graphics.*;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -16,7 +13,7 @@ import su.eqx.iggdrasil.life.GameGeneration.Cell;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
-	 class GameThread extends Thread{
+	class GameThread extends Thread{
 
 		private GameGeneration mGameGeneration;
 
@@ -24,9 +21,16 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
 		private Touchedstate mTouchedState;
 
+		private PointF mTouchedPoint;
+
+		private float mXOffset;
+
+		private float mYOffset;
+
 		private final SurfaceHolder mSurfaceHolder;
 
-		private Paint mPaint;
+		private Paint mGridPaint;
+		private Paint mBorderPaint;
 
 		private Handler mUpdateHandler = new Handler();
 
@@ -46,8 +50,13 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 		GameThread(SurfaceHolder holder) {
 			mSurfaceHolder = holder;
 			mGameGeneration = new GameGeneration();
-			mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-			mPaint.setColor(Color.BLACK);
+
+			mGridPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+			mGridPaint.setColor(Color.BLACK);
+
+			mBorderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+			mBorderPaint.setColor(Color.RED);
+
 		}
 
 		@Override
@@ -85,25 +94,54 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
 			int width = canvas.getWidth();
 			int height = canvas.getHeight();
+
 			if (mGameState == GameState.MODELING) {
 
-				canvas.drawLines(generateGrid(width, height, mGameGeneration.getRowCount(), mGameGeneration.getColumnCount()), mPaint);
+				canvas.drawLines(generateGrid(width, height, mGameGeneration.getRowCount(), mGameGeneration.getColumnCount()), mGridPaint);
 			}
+
+//			canvas.drawLines(generateBorders(width, height), mBorderPaint);
+
 			for (Cell c: mGameGeneration.getAliveCells()) {
 				Point p = getCellCenter(c);
-				canvas.drawCircle(p.x, p.y, 3, mPaint);
+				canvas.drawCircle(p.x, p.y, 3, mGridPaint);
 			}
 		}
 
 		private boolean doTouchEvent(MotionEvent event) {
 			synchronized (mSurfaceHolder) {
-				if (event.getAction() == MotionEvent.ACTION_DOWN) {
-					Point point = getCorrespondingCell(event.getX(), event.getY());
-					mGameGeneration.addOrRemoveCell(point.x, point.y);
-					updateScreen();
-					return true;
-				} else {
+				int action = event.getActionMasked();
+				switch (action) {
+					case (MotionEvent.ACTION_DOWN): {
+						mTouchedState = Touchedstate.TOUCHED;
+						mTouchedPoint = new PointF(event.getX(), event.getY());
+		//					Point point = getCorrespondingCell(event.getX(), event.getY());
+		//					mGameGeneration.addOrRemoveCell(point.x, point.y);
+							updateScreen();
+						return true;
+				}
+					case (MotionEvent.ACTION_MOVE): {
+						mTouchedState = Touchedstate.MOVING;
+						PointF newPoint = new PointF(event.getX(), event.getY());
+						mXOffset += newPoint.x - mTouchedPoint.x;
+						mYOffset += newPoint.y - mTouchedPoint.y;
+						mTouchedPoint = newPoint;
+						updateScreen();
+					}
+
+					case (MotionEvent.ACTION_UP): {
+						if (mTouchedState == Touchedstate.TOUCHED) {
+							Point point = getCorrespondingCell(event.getX(), event.getY());
+							mGameGeneration.addOrRemoveCell(point.x, point.y);
+							updateScreen();
+						}
+						mTouchedState = Touchedstate.FREE;
+						return true;
+				}
+
+					default: {
 					return false;
+					}
 				}
 			}
 		}
@@ -111,6 +149,9 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 		private Point getCorrespondingCell(float x, float y) {
 			float h_step = ((float) getWidth()) / mGameGeneration.getColumnCount();
 			float v_step = ((float) getHeight()) / mGameGeneration.getRowCount();
+
+			x = calcUnoffsetedX(x);
+			y = calcUnoffsetedY(y);
 
 			int column = (int) (x / h_step);
 			int row = (int) (y/ v_step);
@@ -121,15 +162,15 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 			float h_step = ((float) getWidth()) / mGameGeneration.getColumnCount();
 			float v_step = ((float) getHeight()) /mGameGeneration.getRowCount();
 
-			int center_x = (int) (h_step * cell.getColumn() + h_step / 2);
-			int center_y = (int) (v_step * cell.getRow() + v_step / 2);
+			int center_x = (int) calcOffsetedX(h_step * cell.getColumn() + h_step / 2);
+			int center_y = (int) calcOffsetedY(v_step * cell.getRow() + v_step / 2);
 
 			return new Point(center_x, center_y);
 		}
 
 		private float[] generateGrid(int canwas_width, int canvas_height, int row_count, int column_count) {
-			int vertical_lines_count = column_count - 1;
-			int horizontal_lines_count = row_count - 1;
+			int vertical_lines_count = column_count ;
+			int horizontal_lines_count = row_count ;
 
 			float[] grid = new float[4 * (horizontal_lines_count + vertical_lines_count)];
 
@@ -138,24 +179,72 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
 			float current_y = 0;
 			for (int i = 0; i < 4 * horizontal_lines_count; i += 4) {
+				grid[i] = (0);
+				grid[i + 1] = calcOffsetedY(current_y);
+				grid[i + 2] = (canwas_width);
+				grid[i + 3] = calcOffsetedY(current_y);
 				current_y += vertical_step;
-				grid[i] = 0;
-				grid[i + 1] = current_y;
-				grid[i + 2] = canwas_width;
-				grid[i + 3] = current_y;
+//				if (grid[i + 2] < grid[i]) {
+//					float temp = grid[i];
+//					grid[i] = grid[i + 2];
+//					grid[i + 2] = temp;
+//				}
 			}
 
 			float current_x = 0;
 			for (int j = 4 * horizontal_lines_count; j < grid.length; j += 4) {
+				grid[j] = calcOffsetedX(current_x);
+				grid[j + 1] = (0);
+				grid[j + 2] = calcOffsetedX(current_x);
+				grid[j + 3] = (canvas_height);
 				current_x += horizontal_step;
-				grid[j] = current_x;
-				grid[j + 1] = 0;
-				grid[j + 2] = current_x;
-				grid[j + 3] = canvas_height;
 			}
 
 			return grid;
 		}
+
+		private float[] generateBorders(float width, float height) {
+			float min_x = calcOffsetedX(0);
+			float min_y = calcOffsetedY(0);
+			float max_x = calcOffsetedX(width);
+			float max_y = calcOffsetedY(height);
+			float[] borders = {min_x, min_y, max_x, min_y,
+							   min_x, max_y, max_x, max_y,
+							   min_x, min_y, min_x, max_y,
+							   max_x, min_y, max_x, max_y};
+
+			return borders;
+		}
+
+		private float calcOffsetedX(float x) {
+			return calcOffsetedCoord(x, GameView.this.getWidth(), mXOffset);
+		}
+
+		private float calcOffsetedY(float y) {
+			return calcOffsetedCoord(y, GameView.this.getHeight(), mYOffset);
+		}
+
+		private float calcUnoffsetedX(float x) {
+			return calcOffsetedCoord(x, GameView.this.getWidth(), -mXOffset);
+		}
+
+		private float calcUnoffsetedY(float y) {
+			return calcOffsetedCoord(y, GameView.this.getHeight(), -mYOffset);
+		}
+
+		private float calcOffsetedCoord(float coord, float limit, float offset) {
+			if (coord == limit) {
+				return coord;
+			}
+			float offsetedCoord = (coord + offset) % limit;
+			offsetedCoord = (offsetedCoord >= 0) ? offsetedCoord : (offsetedCoord + limit);
+			return offsetedCoord;
+		}
+
+//		private float calcUnoffsetedCoord(float coord, float limit, float offset) {
+//			float unOffCoord = (coord - offset) % limit;
+//			return unOffCoord
+//		}
 	}
 
 	private GameThread mThread;
